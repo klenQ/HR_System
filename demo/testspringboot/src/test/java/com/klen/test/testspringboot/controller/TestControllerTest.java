e5fc83f2db00438c484a8596a1424cb510d803ba
klenQ <51435887+klenQ@users.noreply.github.com>
2021-11-14 23:35:46 +0800

    added some java bean

---
+++ demo/testspringboot/src/test/java/com/klen/test/testspringboot/controller/TestControllerTest.java
@@ -0,0 +1,24 @@
+package com.klen.test.testspringboot.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Description:
+ * @Author: Jianyu Qiu (Kalen)
+ * @CreateTime: 2021/11/14
+ */
+@SpringBootTest
+class TestControllerTest {
+
+    @Autowired
+    private TestController testController;
+
+    @Test
+    void testTest(){
+        testController.test();
+    }
+}
